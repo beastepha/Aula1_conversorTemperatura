9c356caed44b1c6aa66b7624cbbd8b7f2c58e5f5
beastepha <biastephaniemartins@gmail.com>
2023-11-19 19:52:27 -0300

    subindo codigo da imersao dev

---
+++ script.js
@@ -0,0 +1,7 @@
+let valorCelsius = 27;
+let valorFahrenheit = (valorCelsius * 9) / 5 + 32;
+
+let valorKelvin = valorCelsius + 273.15;
+
+console.log("A conversão de Celsius para Fahrenheit é " + valorFahrenheit);
+console.log("A conversão de Celsius para Kelvin é " + valorKelvin);
